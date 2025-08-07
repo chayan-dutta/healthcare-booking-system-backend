d8371176461eaca53d445e0fb5d944d73795a95b
chayandutta <chayan.dutta@mindteck.com>
2025-08-07 22:06:47 +0530

    A simple API added

---
+++ HealthCare.Cloud/HealthCare.Cloud.AuthService/Controllers/PublicAuthController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Collections.Specialized;
+using System.Text;
+
+namespace HealthCare.Cloud.AuthService.Controllers
+{
+    [Tags("Public Auth APIs")]
+    [ApiController]
+    [Route("auth/public")]
+    public class PublicAuthController : ControllerBase
+    {
+        [HttpGet]
+        [Route("getstatus")]
+        public string GetAuthAPIStatus() => "Running"; 
+    }
+}
