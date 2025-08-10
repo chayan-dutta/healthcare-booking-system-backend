d7c26f275e004944a1ac4dc3ff1642ffb8b0faba
chayandutta <chayan.dutta@mindteck.com>
2025-08-10 19:08:45 +0530

    json propertyname added and message property added

--- HealthCare.Common/HealthCare.Common.Models/PublicApiResponse.cs
+++ HealthCare.Common/HealthCare.Common.Models/PublicApiResponse.cs
@@ -1,4 +1,5 @@
 ﻿using System.Net;
+using System.Text.Json.Serialization;
 
 namespace HealthCare.Common.Models;
 
@@ -11,15 +12,24 @@ public class ApiResponse<T> where T : notnull
     /// <summary>
     /// Status code
     /// </summary>
+    [JsonPropertyName("statuscode")]
     public HttpStatusCode Status { get; set; }
+
+    /// <summary>
+    /// Message if any
+    /// </summary>
+    [JsonPropertyName("message")]
+    public string Message { get; set; } = string.Empty;
 
     /// <summary>
     /// Any data
     /// </summary>
+    [JsonPropertyName("data")]
     public T? Data { get; set; }
 
     /// <summary>
     /// Success status
     /// </summary>
+    [JsonPropertyName("isSuccessStatus")]
     public bool IsSuccess { get; set; }
 }
