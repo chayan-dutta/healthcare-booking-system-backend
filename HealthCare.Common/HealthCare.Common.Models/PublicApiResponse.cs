23afd54de62cd405d5857ff7aed5d5a2ec4e6ec5
chayandutta <chayan.dutta@mindteck.com>
2025-08-10 18:42:09 +0530

    Common model for API response

---
+++ HealthCare.Common/HealthCare.Common.Models/PublicApiResponse.cs
@@ -0,0 +1,25 @@
+﻿using System.Net;
+
+namespace HealthCare.Common.Models;
+
+/// <summary>
+/// Common model for API response
+/// </summary>
+/// <typeparam name="T"></typeparam>
+public class ApiResponse<T> where T : notnull
+{
+    /// <summary>
+    /// Status code
+    /// </summary>
+    public HttpStatusCode Status { get; set; }
+
+    /// <summary>
+    /// Any data
+    /// </summary>
+    public T? Data { get; set; }
+
+    /// <summary>
+    /// Success status
+    /// </summary>
+    public bool IsSuccess { get; set; }
+}
