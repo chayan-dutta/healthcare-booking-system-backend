7affdba1b898fb2009d976cce0c39302621333a7
chayandutta <chayan.dutta@mindteck.com>
2025-08-30 20:23:42 +0530

    Add user response

---
+++ HealthCare.Common/HealthCare.Common.Models/User/AddUserResponse.cs
@@ -0,0 +1,17 @@
+﻿namespace HealthCare.Common.Models.User;
+
+/// <summary>
+/// Response model for Adding an user
+/// </summary>
+public class AddUserResponse
+{
+    /// <summary>
+    /// User Id
+    /// </summary>
+    public Guid UserId { get; set; }
+    
+    /// <summary>
+    /// Email id of User
+    /// </summary>
+    public string Email { get; set; } = string.Empty;
+}
